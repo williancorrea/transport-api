f8f85a2258ede7e494fa0ff5e4111dcce2715a3a
Willian Vagner Vicente Correa <willian.vag@gmail.com>
2018-06-17 12:41:03 -0300

    Corrige o timezone das datas

--- src/main/java/br/com/wcorrea/transport/api/ApiApplication.java
+++ src/main/java/br/com/wcorrea/transport/api/ApiApplication.java
@@ -5,11 +5,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
+    }
+
+    /**
+     * Corrige o timezone da datas
+     */
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
 
     @Bean
