a5f996d40c091b276cfff9e68b92ab5de1597f95
Willian Vagner Vicente Correa <willian.vag@gmail.com>
2019-03-02 18:59:13 -0300

    Teste CORs

--- src/main/java/br/com/wcorrea/transport/api/config/cors/CorsFilter.java
+++ src/main/java/br/com/wcorrea/transport/api/config/cors/CorsFilter.java
@@ -25,43 +25,28 @@ public class CorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) resp;
 
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin").trim());
 
+        if (apiProperty.getSecurity().isEnableCors()) {
+            response.setHeader("Access-Control-Allow-Origin", apiProperty.getOriginAllowed().trim());
 
-        if ("OPTIONS".equals(request.getMethod()) && (
-                        request.getHeader("Origin").equalsIgnoreCase(apiProperty.getOriginAllowed()) ||
-                        request.getHeader("Origin").startsWith(apiProperty.getOriginAllowed()))
+            //Bloqueia caso nao tenha a origem permitida
+            if (!request.getHeader("Origin").equalsIgnoreCase(apiProperty.getOriginAllowed()) ||
+                    !request.getHeader("Origin").toLowerCase().startsWith(apiProperty.getOriginAllowed().toLowerCase())) {
+                chain.doFilter(req, resp);
+            }
+        } else {
+            response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin").trim());
+        }
 
-        ) {
+        if ("OPTIONS".equals(request.getMethod())) {
             response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
-            response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept, X-Requested-With");
-            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+            response.setHeader("Access-Control-Max-Age", "3600");//TODO: Verificar se nao esta sendo pouco
 
             response.setStatus(HttpServletResponse.SC_OK);
         } else {
             chain.doFilter(req, resp);
         }
-
-//
-//
-//        if (apiProperty.getSecurity().isEnableCors()) {
-//            response.setHeader("Access-Control-Allow-Origin", apiProperty.getOriginAllowed().trim());
-//
-//            //Bloqueia caso nao tenha a origem permitida
-//            if (!apiProperty.getOriginAllowed().trim().equals(request.getHeader("Origin").trim())) {
-//                chain.doFilter(req, resp);
-//            }
-//        }
-//
-//        if ("OPTIONS".equals(request.getMethod())) {
-//            response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
-//            response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
-//            response.setHeader("Access-Control-Max-Age", "3600");//TODO: Verificar se nao esta sendo pouco
-//
-//            response.setStatus(HttpServletResponse.SC_OK);
-//        } else {
-//            chain.doFilter(req, resp);
-//        }
 
     }
 
