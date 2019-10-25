73ba5e7bcde11feb0244297f1d7f7876307c61e6
Willian Vagner Vicente Correa <willian.vag@gmail.com>
2019-10-25 00:58:26 -0300

    Corrigindo script de criacao do BD

--- src/main/resources/db/migration/mysql/V0013__Criar_tabela_modelo_marca_combustivel_veiculo.sql
+++ src/main/resources/db/migration/mysql/V0013__Criar_tabela_modelo_marca_combustivel_veiculo.sql
@@ -22,6 +22,33 @@ CREATE TABLE veiculo_modelo (
 
 
 
+
+
+CREATE TABLE combustivel (
+    id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(150) NOT NULL,
+    inativo BOOLEAN DEFAULT FALSE,
+
+    data_criacao  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    data_alteracao  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8;
+
+-- -------------------------
+-- COMBUSTIVELS
+-- -------------------------
+INSERT INTO combustivel (nome, inativo) VALUES ('DIESEL S500', false);
+INSERT INTO combustivel (nome, inativo) VALUES ('DIESEL S10', false);
+INSERT INTO combustivel (nome, inativo) VALUES ('GASOLINA', true);
+INSERT INTO combustivel (nome, inativo) VALUES ('ETANOL', true);
+
+
+
+
+
+
+
 CREATE TABLE veiculo (
     id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
     placa VARCHAR(8) NOT NULL,
@@ -60,19 +87,4 @@ CREATE TABLE veiculo (
     data_alteracao  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )
     ENGINE = InnoDB DEFAULT CHARSET = utf8;
-
-
--- -------------------------
--- PERMISSOES
--- -------------------------
-INSERT INTO permissao (nome, descricao) values ('ROLE_ACESSAR_URI_VEICULO', '');
-INSERT INTO permissao (nome, descricao) values ('ROLE_LISTAR_VEICULO', '');
-INSERT INTO permissao (nome, descricao) values ('ROLE_DELETAR_VEICULO', '');
-INSERT INTO permissao (nome, descricao) values ('ROLE_ATUALIZAR_VEICULO', '');
-INSERT INTO permissao (nome, descricao) values ('ROLE_SALVAR_VEICULO', '');
-INSERT INTO usuario_permissao (id_usuario, id_permissao) VALUES ((select id from usuario where email='willian.vag@gmail.com'), (select id from permissao where nome='ROLE_ACESSAR_URI_VEICULO'));
-INSERT INTO usuario_permissao (id_usuario, id_permissao) VALUES ((select id from usuario where email='willian.vag@gmail.com'), (select id from permissao where nome='ROLE_LISTAR_VEICULO'));
-INSERT INTO usuario_permissao (id_usuario, id_permissao) VALUES ((select id from usuario where email='willian.vag@gmail.com'), (select id from permissao where nome='ROLE_DELETAR_VEICULO'));
-INSERT INTO usuario_permissao (id_usuario, id_permissao) VALUES ((select id from usuario where email='willian.vag@gmail.com'), (select id from permissao where nome='ROLE_ATUALIZAR_VEICULO'));
-INSERT INTO usuario_permissao (id_usuario, id_permissao) VALUES ((select id from usuario where email='willian.vag@gmail.com'), (select id from permissao where nome='ROLE_SALVAR_VEICULO'));
 
--- src/main/resources/db/migration/mysql/V0024__Alterar_permissoes_acesso.sql
+++ src/main/resources/db/migration/mysql/V0024__Alterar_permissoes_acesso.sql
@@ -3,3 +3,18 @@
 -- -------------------------
 INSERT INTO permissao (nome, descricao) values ('ROLE_ALTERAR_PERMISSOES_ACESSO', '');
 INSERT INTO usuario_permissao (id_usuario, id_permissao) VALUES ((select id from usuario where email='willian.vag@gmail.com'), (select id from permissao where nome='ROLE_ALTERAR_PERMISSOES_ACESSO'));
+
+
+-- -------------------------
+-- PERMISSOES - COMBUSTIVEL
+-- -------------------------
+INSERT INTO permissao (nome, descricao) values ('ROLE_ACESSAR_URI_VEICULO', '');
+INSERT INTO permissao (nome, descricao) values ('ROLE_LISTAR_VEICULO', '');
+INSERT INTO permissao (nome, descricao) values ('ROLE_DELETAR_VEICULO', '');
+INSERT INTO permissao (nome, descricao) values ('ROLE_ATUALIZAR_VEICULO', '');
+INSERT INTO permissao (nome, descricao) values ('ROLE_SALVAR_VEICULO', '');
+INSERT INTO usuario_permissao (id_usuario, id_permissao) VALUES ((select id from usuario where email='willian.vag@gmail.com'), (select id from permissao where nome='ROLE_ACESSAR_URI_VEICULO'));
+INSERT INTO usuario_permissao (id_usuario, id_permissao) VALUES ((select id from usuario where email='willian.vag@gmail.com'), (select id from permissao where nome='ROLE_LISTAR_VEICULO'));
+INSERT INTO usuario_permissao (id_usuario, id_permissao) VALUES ((select id from usuario where email='willian.vag@gmail.com'), (select id from permissao where nome='ROLE_DELETAR_VEICULO'));
+INSERT INTO usuario_permissao (id_usuario, id_permissao) VALUES ((select id from usuario where email='willian.vag@gmail.com'), (select id from permissao where nome='ROLE_ATUALIZAR_VEICULO'));
+INSERT INTO usuario_permissao (id_usuario, id_permissao) VALUES ((select id from usuario where email='willian.vag@gmail.com'), (select id from permissao where nome='ROLE_SALVAR_VEICULO'));
